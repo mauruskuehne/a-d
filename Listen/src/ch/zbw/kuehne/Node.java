0398d2b3b2d645139e6d5541ce4593748d33fe4f
Maurus Kühne <maumau@Mauruss-MacBook-Pro.local>
2015-04-28 13:23:50 +0200

    added Last property on node

--- Listen/src/ch/zbw/kuehne/Node.java
+++ Listen/src/ch/zbw/kuehne/Node.java
@@ -3,10 +3,17 @@ package ch.zbw.kuehne;
 public class Node<T> {
 	private T _item;
 	private Node<T> _next;
+	private Node<T> _last;
 	
 	public Node(T item, Node<T> next) {
 		_item = item;
 		_next = next;
+	}
+	
+	public Node(T item, Node<T> next, Node<T> last) {
+		_item = item;
+		_next = next;
+		_last = last;
 	}
 	
 	public T getItem() {
@@ -19,5 +26,13 @@ public class Node<T> {
 	
 	public void setNext(Node<T> next) {
 		_next = next;
+	}
+	
+	public Node<T> getLast() {
+		return _last;
+	}
+	
+	public void setLast(Node<T> last) {
+		_last = last;
 	}
 }
